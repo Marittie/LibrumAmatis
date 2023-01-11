0ae16c30479a1168cb06eb8477390ec7234514f5
Marittie <mario.castaldo1992@gmail.com>
2023-01-11 10:25:37 +0000

    Create the first book post on the books page

--- books/admin.py
+++ books/admin.py
@@ -6,7 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Book)
 class BookAdmin(SummernoteModelAdmin):
     """ A class to display books posts to admin """
-    list_display = ('book_title', 'updated_on')
+    list_display = ('book_title', 'writer', 'updated_on')
     search_fields = ['book_title', 'content']
     list_filter = ('updated_on',)
     summernote_fields = ('content')
