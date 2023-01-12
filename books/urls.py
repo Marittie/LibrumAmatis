f56f52a981ecbf5cd8303555d28b4fa5ae923d4f
Marittie <mario.castaldo1992@gmail.com>
2023-01-12 15:42:45 +0000

    Add forms to like and unlike a book

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('books/', views.BookList.as_view(), name='books'),
     path('<slug:slug>/', views.BookDetail.as_view(), name='books_detail'),
+    path('like/<slug:slug>', views.BookLike.as_view(), name='book_like'),
     ]
--- books/views.py
+++ books/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import (ListView, DetailView)
+from django.views.generic import (ListView, DetailView, CreateView)
+from django.http import HttpResponseRedirect
 from .models import Book
+from .forms import ReviewForm
 
 
 class BookList(ListView):
@@ -26,7 +28,55 @@ class BookDetail(DetailView):
             {
                 "book": book,
                 "reviews": reviews,
-                "liked": liked
+                "liked": liked,
+                "review_form": ReviewForm()
             }
         
         )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Book.objects.filter(status=1)
+        book = get_object_or_404(queryset, slug=slug)
+        reviews = book.reviews.order_by("-created_on")
+        liked = False
+        if book.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        review_form = ReviewForm(data=request.POST)
+
+        if review_form.is_valid():
+            review_form.instance.email = request.user.email
+            review_form.instance.name = request.user.username
+            review = review_form.save(commit=False)
+            review.book = book
+            review.save()
+        else:
+            review_form = ReviewForm()
+
+        return render(
+            request,
+            "books_detail.html",
+            {
+                "book": book,
+                "reviews": reviews,
+                "reviewed": True,
+                "liked": liked,
+                "review_form": ReviewForm()
+            }
+        
+        )
+
+
+class BookLike(CreateView):
+    """ A view to like a book """
+
+    def post(self, request, slug):
+        """ A function to add or remove a like """
+        book = get_object_or_404(Book, slug=slug)
+
+        if book.likes.filter(id=request.user.id).exists():
+            book.likes.remove(request.user)
+        else:
+            book.likes.add(request.user)
+
+        return HttpResponseRedirect(self.request.META.get('HTTP_REFERER'))
