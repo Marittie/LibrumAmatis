200983797eab233104924a7285aade78e0941ab6
Marittie <mario.castaldo1992@gmail.com>
2023-01-18 12:18:49 +0000

    adjust conf. in summernote settings to allow forms to shrink on smaller screens

--- librumamatis/settings.py
+++ librumamatis/settings.py
@@ -157,3 +157,4 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SUMMERNOTE_CONFIG = { 'width': '100%', 'height': '400px', }
